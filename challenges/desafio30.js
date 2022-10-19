04597960122e103929bbee5a0d0c3404c486bedc
DevPBDias <devpbdias@gmail.com>
2022-10-19 19:53:19 -0300

    feat: query unset, remover campo do doc

--- challenges/desafio30.js
+++ challenges/desafio30.js
@@ -6,4 +6,4 @@ db.produtos.updateOne(
     { nome: "Big Mac" },
     { $unset: { curtidas: "" } },
 );
-db.produtos.find({ nome: { $ne: "Big Mac" } }, { nome: 1, curtidas: 1, _id: 0 });
+db.produtos.find({}, { nome: 1, curtidas: 1, _id: 0 });
